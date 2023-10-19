a33b2ee9f90034edbe7d91d19b023cfd7b023a12
Ajo <17722952+jo-0@users.noreply.github.com>
2023-10-19 15:59:23 +0530

    add a healthcheck endpoint

--- services/backend/project/__init__.py
+++ services/backend/project/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask, jsonify
+# from flask.views import View
 
 
 app = Flask(__name__)
 
 
-@app.route("/generate/")
+@app.route("/api/")
 def hello_world():
-    return jsonify(hi="Sopi")
+    return jsonify(hello="World")
+
+
+# class Health(View):
+#     def get(self):
+#         data = {"success": True, "message": "healthy"}
+#         return jsonify(**data, status=200, mimetype='application/json')
+
+# app.add_url_rule("/healthcheck", view_func=Health.as_view("health_check"))
+
+
+@app.route("/healthcheck", strict_slashes=False)
+def health_check():
+    data = {"success": True, "message": "healthy"}
+    return jsonify(**data, status=200, mimetype='application/json')
